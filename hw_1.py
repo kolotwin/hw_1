3b2767697e0b09e8449ff77cfa4bf4e45ebebd16
Egor Kolotvin <72461324+kolotwin@users.noreply.github.com>
2022-02-13 03:30:40 +0300

    HomeWork №1 is complited

--- hw_1.py
+++ hw_1.py
@@ -74,7 +74,6 @@ print(selected_column)
 def get_unique_rows(X):
     X_unique = np.unique(X, axis=0)
     return X_unique
-
 X = np.random.randint(4, 6, size=(10,3))
 print(X)
 print(get_unique_rows(X))
